ce892f5783b5c88ebee48f592c02c10e42df679a
John Asto <john.azt.dev@gmail.com>
2023-12-26 19:49:24 -0300

    add event emitter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { TodoFormComponent } from './ui/todo-form.component';
+import Todo from '../shared/interfaces/todo';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [TodoFormComponent],
   template: `
-    <app-todo-form></app-todo-form>
+    <h2 class="text-lg font-semibold text-slate-800 py-5 italic">To Do</h2>
+    <app-todo-form (todoSubmitted)="createTodo($event)" />
   `,
-  styles: ``
+  styles: ``,
 })
 export default class HomeComponent {
-
+  createTodo(todo: Todo) {
+    console.log(todo);
+  }
 }
--- src/app/home/ui/todo-form.component.ts
+++ src/app/home/ui/todo-form.component.ts
@@ -1,30 +1,38 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import Todo from '../../shared/interfaces/todo';
 
 @Component({
   standalone: true,
   selector: 'app-todo-form',
   template: `
-    <form class="pt-10 flex flex-col gap-3 space-y-3" [formGroup]="todoForm">
+    <form
+      class=" flex flex-col gap-3 space-y-4"
+      [formGroup]="todoForm"
+      (ngSubmit)="todoSubmitted.emit(todoForm.getRawValue())"
+    >
       <input
-        class="shadow-md p-3"
+        class="shadow-md p-3 rounded-md"
         type="text"
         formControlName="title"
         placeholder="title..."
       />
       <input
         type="text"
-        class="shadow-md p-3"
+        class="shadow-md p-3 rounded-md"
         formControlName="description"
         placeholder="description..."
       />
-      <button class="bg-slate-900 text-white rounded-md p-3" type="submit">Add todo</button>
+      <button class="bg-slate-900 text-white rounded-md p-3 disabled:opacity-45" type="submit" [disabled]="!todoForm.valid">
+        Add todo
+      </button>
     </form>
   `,
   imports: [ReactiveFormsModule],
 })
 export class TodoFormComponent {
   private fb = inject(FormBuilder);
+  @Output() todoSubmitted = new EventEmitter<Todo>();
 
   todoForm = this.fb.nonNullable.group({
     title: ['', Validators.required],
