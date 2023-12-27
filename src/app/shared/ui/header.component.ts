04af56a2ec8ccee719d37a8e45436167382f1837
John Asto <john.azt.dev@gmail.com>
2023-12-27 20:55:17 -0300

    add id activatedroute

--- src/app/shared/ui/header.component.ts
+++ src/app/shared/ui/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [RouterLink],
   template: `
     <header class="bg-slate-900 shadow-md shadow-amber-200">
       <div class="container  py-4 md:py-6">
-        <span class="font-semibold text-amber-100 text-lg"> ToDo App</span>
+        <span class="font-semibold text-amber-100 text-lg cursor-pointer" routerLink="/"> ToDo App</span>
       </div>
     </header>
   `,
