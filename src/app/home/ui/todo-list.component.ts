b94b218a2ad2ca464e4f891676647a82657d828f
John Asto <john.azt.dev@gmail.com>
2023-12-26 21:52:02 -0300

    add new list styles

--- src/app/home/ui/todo-list.component.ts
+++ src/app/home/ui/todo-list.component.ts
@@ -5,10 +5,10 @@ import Todo from '../../shared/interfaces/todo';
   standalone: true,
   selector: 'app-todo-list',
   template: `
-    <h4 class="text-lg font-semibold text-slate-800 py-5">Todo List</h4>
-    <ul class="space-x-7">
+    <h4 class="text-lg font-semibold text-slate-800 pt-10">Todo List</h4>
+    <ul class="py-5">
       @for (todo of todos; track $index) {
-      <li class="flex flex-col bg-amber-100 text-slate-800 px-4 py-6 rounded-md font-semibold transition-opacity duration-75">
+      <li class="flex flex-col bg-amber-100 text-slate-800 px-4 py-6 rounded-md font-semibold mb-4">
           <a class="text-lg" href="">{{ todo.title }}</a>
           <small class="text-gray-500">{{ todo.description }}</small>
       </li>
